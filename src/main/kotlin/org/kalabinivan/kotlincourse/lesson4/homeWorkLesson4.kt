fc8daea02bd221c8b858bf8c118a0543c8e3d94e
Ivan <i.kalabin.qa@gmail.com>
2024-09-15 00:19:04 +0200

    homeWorkLesson4

---
+++ src/main/kotlin/org/kalabinivan/kotlincourse/lesson4/homeWorkLesson4.kt
@@ -0,0 +1,50 @@
+package org.kalabinivan.kotlincourse.lesson4
+
+val myByte1: Byte = 42
+val myLong1: Long = 98765432123456789L
+val myFloat1: Float = 23.45f
+val myDouble1: Double = 0.123456789
+val myString1: String = "Kotlin & Java"
+val myBoolean1: Boolean = false
+val myChar1: Char = 'c'
+val myInt2: Int = 500
+val myLong2 = 4294967296L
+val myFloat2: Float = 18.0f
+val myDouble2: Double = -0.001
+val myString2: String = "OpenAI"
+
+// “true” // это должен быть String, но установлены неверные кавычки
+
+val myChar2: Char = '9'
+val myInt11: Int = 2048
+val myLong3: Long = 10000000000L
+val myFloat3: Float = 5.75f
+val myDouble3: Double = 1.414
+val myString3: String = "Artificial Intelligence"
+val myBoolean2: Boolean = false
+val myChar3: Char = '@'
+val myInt12: Int = 1024
+val myLong4: Long = 1234567890123L
+val myFloat4: Float = 10.01f
+val myDouble4: Double = -273.15
+val myString4: String = "SpaceX"
+val myBoolean3: Boolean = true
+
+// “🤯” // в эмодзи обычно зашито несколько символов, плюс здесь какие-то кривые кавычки. Эмодзи вряд ли стоит закладывать
+// в переменные
+
+
+// ‘65535’ // здесь определенно подразумевается переменная стринг, так как есть кавычки, но синтаксис Kotlin, такие
+// кавычки не принимает
+
+val myLong5: Long = 72057594037927935L
+val myFloat5: Float = 2.71828f
+val myDouble5: Double = 101.0101
+val myString5: String = "Quantum Computing"
+val myBoolean4: Boolean = false
+val myChar4: Char = 'x'
+val myShort1: Short = 314
+val myLong6: Long = 123456789123456789L
+val myFloat6: Float = 6.626f
+val myDouble6: Double = 0.007
+val myString6: String = "Android Studio"
