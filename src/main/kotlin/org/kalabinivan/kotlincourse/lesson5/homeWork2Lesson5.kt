4e39d75bc2a842f2705e322a12640a6ce0c9825d
Ivan <i.kalabin.qa@gmail.com>
2024-09-19 00:06:32 +0200

    homeWorkLesson5

--- src/main/kotlin/org/kalabinivan/kotlincourse/lesson5/homeWork2Lesson5.kt
+++ src/main/kotlin/org/kalabinivan/kotlincourse/lesson5/homeWork2Lesson5.kt
@@ -3,7 +3,7 @@ package org.kalabinivan.kotlincourse.lesson5
 fun main() {
     printSoundInt(fadingKoef = 0.8)
     printSoundInt(fadingKoef = null)
-    printDeliveryPrice(70.0,)
+    printDeliveryPrice(70.0)
     printDeliveryPrice(null)
     printMeteoMeasurements(700)
     printMeteoMeasurements(null)
@@ -11,16 +11,16 @@ fun main() {
 
 }
 
-fun printSoundInt(SoundInt: Double = 10.0, fadingKoef: Double?){
+fun printSoundInt(soundInt: Double = 10.0, fadingKoef: Double?){
 
     val defaultFadKoef = 0.5
-    println(SoundInt * (fadingKoef ?: defaultFadKoef))
+    println(soundInt * (fadingKoef ?: defaultFadKoef))
 }
 
 fun printDeliveryPrice (cargoCost: Double?, insuranceKoef: Double = 0.5) {
-    val defaultCargoCost: Double = 50.0
+    val defaultCargoCost = 50.0
 
-    var insurance = ((cargoCost ?: defaultCargoCost) * insuranceKoef)
+    val insurance = ((cargoCost ?: defaultCargoCost) * insuranceKoef)
 
     println((cargoCost ?: defaultCargoCost) + insurance)
 }
