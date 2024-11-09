a46670c041b27029ae9f9979a26f1c615ebe21d4
Ivan <i.kalabin.qa@gmail.com>
2024-11-09 21:01:21 +0100

    fixes

--- src/main/kotlin/org/kalabinivan/kotlincourse/lesson17/homework/Materials.kt
+++ src/main/kotlin/org/kalabinivan/kotlincourse/lesson17/homework/Materials.kt
@@ -1,5 +1,7 @@
 package org.kalabinivan.kotlincourse.lesson17.homework
 
+import org.kalabinivan.kotlincourse.lesson17.homework.ExtractLast.ExtractMiddle
+
 abstract class Materials {
     private val materials = mutableListOf<String>()
 
@@ -20,3 +22,75 @@ abstract class Materials {
     }
 
 }
+
+class ExtractFirst(): Materials() {
+    fun extract(): String {
+        val extracted  = extractMaterial()
+        if (extracted.isEmpty()) {
+            return ""
+        }
+        if (extracted.size == 1){
+            return extracted[0]
+        }
+        for (i in 1 until extracted.size) {
+            addMaterial(extracted[i])
+        }
+        return extracted[0]
+    }
+}
+
+class ExtractLast(): Materials() {
+    fun extract(): String {
+        val extracted = extractMaterial()
+        if (extracted.isEmpty()) {
+            return ""
+        }
+        if (extracted.size == 1) {
+            return extracted[0]
+        }
+        for (i in 0 until extracted.size - 1) {
+            addMaterial(extracted[i])
+        }
+        return extracted.last()
+    }
+
+
+    class ExtractMiddle() : Materials() {
+        fun extract(): String {
+            val extracted = extractMaterial()
+            if (extracted.isEmpty()) {
+                return ""
+            }
+            if (extracted.size == 1) {
+                return extracted[0]
+            }
+            val middleIndex = extracted.size / 2
+            for (i in extracted.indices) {
+                if (i == middleIndex) continue
+                addMaterial(extracted[i])
+            }
+            return extracted[middleIndex]
+        }
+    }
+}
+
+fun main() {
+    val ordinalNumbers = listOf(
+        "first", "second", "third", "fourth", "fifth",
+        "sixth", "seventh", "eighth", "ninth", "tenth",
+        "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth",
+        "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth"
+    )
+
+    val m1 = ExtractMiddle()
+    ordinalNumbers.forEach { m1.addMaterial(it) }
+    println(m1.extract())
+    m1.printContainer()
+}
+
+
+
+
+
+
+
--- src/main/kotlin/org/kalabinivan/kotlincourse/lesson18/lesson18.kt
+++ src/main/kotlin/org/kalabinivan/kotlincourse/lesson18/lesson18.kt
@@ -1,43 +1,103 @@
 package org.kalabinivan.kotlincourse.lesson18
 
-//object Colors {
-//    const val RESET = "\u001B[0m"
-//    const val BLACK = "\u001B[30m"
-//    const val RED = "\u001B[31m"
-//    const val GREEN = "\u001B[32m"
-//    const val YELLOW = "\u001B[33m"
-//    const val BLUE = "\u001B[34m"
-//    const val PURPLE = "\u001B[35m"
-//    const val CYAN = "\u001B[36m"
-//    const val WHITE = "\u001B[37m"
-//}
-//
-//object Background {
-//    const val RESET = "\u001B[0m"
-//    const val BLACK = "\u001B[40m"
-//    const val RED = "\u001B[41m"
-//    const val GREEN = "\u001B[42m"
-//    const val YELLOW = "\u001B[43m"
-//    const val BLUE = "\u001B[44m"
-//    const val PURPLE = "\u001B[45m"
-//    const val CYAN = "\u001B[46m"
-//    const val WHITE = "\u001B[47m"
-//}
-//
-//fun printColored(text: String, color: String, backgroundColor: String = "") {
-//    println("$color$backgroundColor$text${Colors.RESET}")
-//}
-//
-//
-//abstract class MusicalInstrument {
-//
-//    abstract fun playNote(note: String)
-//
-//    protected fun shortNote(note: String, color: String) {
-//        printColored("Play short note $note", color)
-//    }
-//
-//    protected fun longNote(note: String, color: String) {
-//        printColored("Play long note $note", color)
-//    }
-//}
+import org.kalabinivan.kotlincourse.lesson15.Calculator
+
+object Colors {
+    const val RESET = "\u001B[0m"
+    const val BLACK = "\u001B[30m"
+    const val RED = "\u001B[31m"
+    const val GREEN = "\u001B[32m"
+    const val YELLOW = "\u001B[33m"
+    const val BLUE = "\u001B[34m"
+    const val PURPLE = "\u001B[35m"
+    const val CYAN = "\u001B[36m"
+    const val WHITE = "\u001B[37m"
+}
+
+object Background {
+    const val RESET = "\u001B[0m"
+    const val BLACK = "\u001B[40m"
+    const val RED = "\u001B[41m"
+    const val GREEN = "\u001B[42m"
+    const val YELLOW = "\u001B[43m"
+    const val BLUE = "\u001B[44m"
+    const val PURPLE = "\u001B[45m"
+    const val CYAN = "\u001B[46m"
+    const val WHITE = "\u001B[47m"
+}
+
+fun printColored(text: String, color: String, backgroundColor: String = "") {
+    println("$color$backgroundColor$text${Colors.RESET}")
+}
+
+
+abstract class MusicalInstrument {
+
+    abstract fun playNote(note: String)
+
+    protected fun shortNote(note: String, color: String) {
+        printColored("Play short note $note", color)
+    }
+
+    protected fun longNote(note: String, color: String) {
+        printColored("Play long note $note", color)
+    }
+}
+
+class Cello : MusicalInstrument() {
+    override fun playNote(note: String) {
+        longNote(note, Colors.BLACK)
+    }
+
+}
+
+class Piano : MusicalInstrument() {
+    override fun playNote(note: String) {
+        shortNote(note, Colors.CYAN)
+    }
+}
+
+class Flute : MusicalInstrument() {
+    override fun playNote(note: String) {
+        longNote(note, Colors.RESET)
+    }
+
+}
+
+class Calculator () {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sum (a: Double, b: Double): Double {
+        return a + b
+    }
+
+    fun sum(a: String, b: String): String {
+        return a + b
+    }
+
+    fun sum(a: List<Int>, b: List<Int>): List<Int> {
+        return a + b
+    }
+
+    fun sum (a: String, b: Boolean): String {
+        return a + b.toString()
+    }
+}
+
+fun main() {
+//    Cello().playNote("Do")
+//    Piano().playNote("Re")
+//    Flute().playNote("Mi")
+    val orchestra: List<MusicalInstrument> = listOf(Cello(), Piano(), Flute())
+    orchestra.forEach { it.playNote("Do") }
+
+    val calculator = org.kalabinivan.kotlincourse.lesson18.Calculator()
+    println(calculator.sum(1,2))
+    println(calculator.sum(1.0,2.0))
+    println(calculator.sum("1","2"))
+    println(calculator.sum(listOf(1,2), listOf(1,2)))
+    println(calculator.sum("1",true))
+}
+
