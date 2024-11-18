b8b3be26bd49a8a03f06a4f4bbe0e7a5129b664c
Ivan <i.kalabin.qa@gmail.com>
2024-11-18 13:17:02 +0100

    lessons

---
+++ src/main/kotlin/org/kalabinivan/kotlincourse/lesson22/lesson22/lesson22.kt
@@ -0,0 +1,17 @@
+package org.kalabinivan.kotlincourse.lesson22.lesson22
+
+fun main() {
+    val result = "String with spaces".removeSpaces()
+    println(result)
+
+    val s: List<Int?> = listOf(1, null)
+    s.isElementsNullOrEmpty()
+}
+
+fun String.removeSpaces(): String {
+    return replace(" ", "")
+}
+
+fun <T> List<T?>.isElementsNullOrEmpty(): Boolean {
+    return this.all { it == null } || this.isEmpty()
+}
